26401cedc81642f99c69ead8dc76ed536a863aa5
Erazem Kokot <mail@erazem.eu>
2022-03-17 21:49:37 +0100

    Implement NewFragment.kt

--- app/src/main/java/si/uni_lj/fri/pbd/miniapp1/NewFragment.kt
+++ app/src/main/java/si/uni_lj/fri/pbd/miniapp1/NewFragment.kt
@@ -1,59 +1,107 @@
 package si.uni_lj.fri.pbd.miniapp1
 
+import android.app.Activity.RESULT_OK
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
+import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
+import java.util.Date
+import java.text.SimpleDateFormat
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class NewFragment : Fragment(R.layout.fragment_new) {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [NewFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class NewFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var memoImage: ImageView? = null
+    private val IMAGE_CAPTURE_REQUEST_CODE = 1
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new, container, false)
-    }
+        Log.d("NewFragment", "Created new fragment")
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NewFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NewFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        val view = inflater.inflate(R.layout.fragment_new, container, false)
+        memoImage = view.findViewById(R.id.newMemoImage)
+        val memoTitle = view.findViewById<TextView>(R.id.newMemoTitle)
+        val memoText = view.findViewById<TextView>(R.id.newMemoText)
+        val takePhotoButton = view.findViewById<Button>(R.id.newTakeImageButton)
+        val saveMemoButton = view.findViewById<Button>(R.id.newSaveMemoButton)
+
+        takePhotoButton.setOnClickListener {
+            Log.d("NewFragment", "Taking new photo")
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            try {
+                startActivityForResult(takePictureIntent, IMAGE_CAPTURE_REQUEST_CODE)
+            } catch (e: ActivityNotFoundException) {
+                Snackbar.make(view, R.string.memo_camera_error, BaseTransientBottomBar.LENGTH_LONG).show()
+            }
+        }
+
+        saveMemoButton.setOnClickListener {
+            Log.d("NewFragment", "Clicked on save memo")
+
+            if (memoImage?.tag == null) {
+                Log.d("NewFragment", "Null tag")
+            }
+
+            // Check if any field is empty
+            if (memoTitle.text.isEmpty() || memoText.text.isEmpty() || memoImage?.tag == null) {
+                if (memoTitle.text.isEmpty()) memoTitle.error = getString(R.string.memo_title_empty_error)
+                if (memoText.text.isEmpty()) memoText.error = getString(R.string.memo_text_empty_error)
+                if (memoImage?.tag == null) Snackbar.make(view, R.string.memo_image_empty_error, BaseTransientBottomBar.LENGTH_LONG).show()
+            } else {
+                Log.d("NewFragment", "Else")
+
+                val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE)
+                val memoId = sharedPref?.getInt("count", 0) as Int
+                val bitmap = memoImage?.drawable as BitmapDrawable
+
+                val memo = MemoModel(
+                    id = memoId,
+                    title = memoTitle.text.toString(),
+                    timestamp = SimpleDateFormat("dd. MM. yy, hh:mm:ss").format(Date()),
+                    text = memoText.text.toString(),
+                    image = bitmap.bitmap
+                )
+
+                Log.d("NewFragment", "Adding memo '${memo.id}', '${memo.title}', '${memo.timestamp}', '${memo.text}'")
+
+                val jsonObject = memoToJson(memo)
+
+                with (sharedPref.edit()) {
+                    this?.putString("$memoId", jsonObject.toString())
+                    this?.putInt("count", memoId + 1)
+                    this?.commit()
+                }
+
+                parentFragmentManager.beginTransaction().apply {
+                    replace(R.id.fragment_container, ListFragment())
+                    commit()
                 }
             }
+        }
+
+        return view
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == IMAGE_CAPTURE_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+            val imageBitmap = data.extras?.get("data") as Bitmap
+            memoImage?.setImageBitmap(imageBitmap)
+            memoImage?.tag = "Nonempty" // Random text to make tag not null
+        } else {
+            Log.d("NewFragment", "Error occurred while taking image with camera")
+        }
     }
 }
