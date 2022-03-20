c0b21f86c22f1ca71de714ce48c69e3b0c1cfdbe
Erazem Kokot <mail@erazem.eu>
2022-03-20 21:57:57 +0100

    Add code attribution

--- app/src/main/java/si/uni_lj/fri/pbd/miniapp1/MainActivity.kt
+++ app/src/main/java/si/uni_lj/fri/pbd/miniapp1/MainActivity.kt
@@ -3,6 +3,11 @@ package si.uni_lj.fri.pbd.miniapp1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/* ATTRIBUTION:
+    Parts of the code in this project have either been copied from or inspired by code from
+    previous labs, PDFs published on Učilnica or from examples on https://developers.android.com.
+ */
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
